dc59e55d3c8b7778e725806bc5126e305c4b5ece
Timothy Wang <curioustimwang@gmail.com>
2022-02-17 09:14:37 -0800

    committed changes to finalze module

--- Lab11.py
+++ Lab11.py
@@ -5,62 +5,38 @@ import math
 
 def PrimMST(G):
     """G is represented as an adjacency matrix"""
-    keys = dict()
+
+    # initializes a list of the smallest weight edge connecting that vertex
+    minimum_weights = list(999999999999 for x in range(len(G)))
+
+    # initializes a list of the preceding node of the current node
+    connections = list(None for x in range(len(G)))
+
+    # initializes starting node as 0, no parent node as it is the head of the tree
+    minimum_weights[0] = 0
+    included = set()
+
     for x in range(len(G)):
-        keys[x] = [99999999999, None]   # some arbitrary very large value
 
-    queue = set()
-    cur = 0
+        # finds the closest node in the graph that
+        closest_value = 9999999999999999
+        closest_node = None
+        for y in range(len(G)):
+            if minimum_weights[y] < closest_value and y not in included:
+                closest_value = minimum_weights[y]
+                closest_node = y
 
-    while len(queue) < len(G):
-        for x in range(len(G)):
-            if G[cur][x] != 0:  # if there exists an edge between cur and x
-                if x not in queue and G[cur][x] < keys[cur][0]:
-                    keys[cur][1] = x
-                    keys[cur][0] = G[cur][x]
-        queue.add(cur)
+        included.add(closest_node)
 
-        closest = 99999999999999
-        for x in range(len(G)):
-            if G[x][0] < closest and x not in queue:
-                cur = x
-                closest = keys[x][0]
-        print(keys)
+        for y in range(len(G)):
+            if G[closest_node][y] > 0 and y not in included and G[closest_node][y] < minimum_weights[y]:
+                minimum_weights[y] = G[closest_node][y]
+                connections[y] = closest_node
 
-    return keys
+    return list(sorted([x, connections[x]]) + [minimum_weights[x]] for x in range(len(G)))
 
 
 def test1():
-    matrix = list(list(0 for x in range(7)) for y in range(7))
-    matrix[0][1] = 7
-    matrix[1][0] = 7
-    matrix[1][2] = 8
-    matrix[2][1] = 8
-    matrix[0][3] = 5
-    matrix[3][0] = 5
-    matrix[1][3] = 9
-    matrix[3][1] = 9
-    matrix[1][4] = 7
-    matrix[4][1] = 7
-    matrix[2][4] = 5
-    matrix[4][2] = 5
-    matrix[3][4] = 15
-    matrix[4][3] = 15
-    matrix[3][5] = 6
-    matrix[5][3] = 6
-    matrix[5][6] = 11
-    matrix[6][5] = 11
-    matrix[6][4] = 9
-    matrix[4][6] = 9
-    matrix[4][5] = 8
-    matrix[5][4] = 8
-    min_tree = PrimMST(matrix)
-    print('Edge       Weight')
-    for x in range(7):
-        print('({}, {})       {}'.format(min_tree[x][1], x, min_tree[x][0]))
-
-
-def test2():
     matrix = [[0, 7, 0, 5, 0, 0, 0],
               [7, 0, 8, 9, 7, 0, 0],
               [0, 8, 0, 0, 5, 0, 0],
@@ -70,13 +46,14 @@ def test2():
               [0, 0, 0, 0, 9, 11, 0]]
     min_tree = PrimMST(matrix)
     print('Edge        Weight')
-    committed = list()
+
+    total = 0
     for x in range(7):
-        if min_tree[x][1] not in committed or x not in committed:
+        if min_tree[x][1] is not None and min_tree[x][0] is not None:
             print('({}, {})       {}'.format(
-                min_tree[x][1], x, min_tree[x][0]))
-            committed.append(min_tree[x][1])
-            committed.append(x)
+                min_tree[x][0], min_tree[x][1], min_tree[x][2]))
+            total += min_tree[x][2]
+    print('MST Weight: {}'.format(total))
 
 
-test2()
+test1()
